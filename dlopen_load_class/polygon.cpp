f0eee29207ef21dc00fc419f1ec4c750c8ba56ef
wangxu <xuwang_ly@163.com>
2017-04-27 14:22:47 +0800

    rm polygon.cpp

--- dlopen_load_class/polygon.cpp
+++
@@ -1,9 +0,0 @@
- #include "polygon.h"
-polygon::polygon() { //set_side_length(0); }
-polygon::~polygon() { }
-void polygon::set_side_length(double side_length) { 
-	printf("polygon side_length: %f\n\n",side_length); 
-	side_length_ = side_length; 
-	printf("polygon side_length_: %f\n\n",side_length_); 
-}
-double polygon::area() const { return 0; }
